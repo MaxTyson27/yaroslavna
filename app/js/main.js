7aea71e500a339dd0bf91d0a4b0e34de8becd052
MaxTyson27 <72412722+MaxTyson27@users.noreply.github.com>
2022-11-16 18:23:47 +0300

    asdad

--- app/js/main.js
+++ app/js/main.js
@@ -1,57 +1,55 @@
 $(function () {
-
-  $(".menu a").on("click", function (event) {
+  $(".menu a, .navigate").on("click", function (event) {
     event.preventDefault();
-    
+
     // $('body').css('overflow','visible')
     // menu.removeClass('active')
     // burger.removeClass('active')
 
-		const id  = $(this).attr('href'),
-			top = $(id).offset().top;
-    $('body,html').animate({scrollTop: top}, 1500);
-    
-    
+    const id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
   });
 
-  $('.advantages__slider').slick({
+  $(".advantages__slider").slick({
     dots: false,
     arrows: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    prevArrow: '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13" fill="none"> <path d="M1 1L7 6.5L1 12" stroke="#333333" stroke-width="0.5"/></svg></button>',
-    nextArrow: '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13" fill="none"> <path d="M1 1L7 6.5L1 12" stroke="#333333" stroke-width="0.5"/></svg></button>',
-  })
-
+    prevArrow:
+      '<button type="button" class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13" fill="none"> <path d="M1 1L7 6.5L1 12" stroke="#333333" stroke-width="0.5"/></svg></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="8" height="13" viewBox="0 0 8 13" fill="none"> <path d="M1 1L7 6.5L1 12" stroke="#333333" stroke-width="0.5"/></svg></button>',
+  });
 
   const setModalActive = () => {
-    const openButtons = $('.modal-btn')
-    const modal = $('.modal')
-    const modalCloseBg = $('.modal-close')
+    const openButtons = $(".modal-btn");
+    const modal = $(".modal");
+    const modalCloseBg = $(".modal-close");
 
     const bodyNonScroll = (modalActive) => {
-      modalActive ? $('body').css('overflow', 'hidden') : $('body').css('overflow', 'visible')
-    }
+      modalActive
+        ? $("body").css("overflow", "hidden")
+        : $("body").css("overflow", "visible");
+    };
 
     const closeModal = () => {
-      modalCloseBg.on('click', () => {
-        modal.removeClass('active')
-        modalCloseBg.removeClass('active')
-        bodyNonScroll(false)
-      })
-    }
+      modalCloseBg.on("click", () => {
+        modal.removeClass("active");
+        modalCloseBg.removeClass("active");
+        bodyNonScroll(false);
+      });
+    };
 
-    openButtons.each( function () {
-      $(this).on('click', (e) => {
-        modal.addClass('active')
-        modalCloseBg.addClass('active')
-        bodyNonScroll(true)
-        closeModal()
-      })
-    })
+    openButtons.each(function () {
+      $(this).on("click", (e) => {
+        modal.addClass("active");
+        modalCloseBg.addClass("active");
+        bodyNonScroll(true);
+        closeModal();
+      });
+    });
+  };
 
-  }
-
-
-  setModalActive()
+  setModalActive();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,96 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const { src, dest, watch, parallel, series } = require("gulp");
 
-const pug = require('gulp-pug');
-const sass = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
-const imagemin = require('gulp-imagemin');
-const del = require('del');
-const browserSync = require('browser-sync').create();
-
+const pug = require("gulp-pug");
+const sass = require("gulp-sass")(require("sass"));
+const concat = require("gulp-concat");
+const autoprefixer = require("gulp-autoprefixer");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const del = require("del");
+const browserSync = require("browser-sync").create();
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'app/'
+      baseDir: "app/",
     },
-    notify: false
-  })
+    notify: false,
+  });
 }
 
 function pugGulp() {
-  return src('app/pug/*.pug')
+  return src("app/pug/*.pug")
     .pipe(
       pug({
-        pretty: true
+        pretty: true,
       })
     )
-    .pipe(dest('app'))
-    .pipe(browserSync.stream())
+    .pipe(dest("app"))
+    .pipe(browserSync.stream());
 }
 
 function styles() {
-  return src('app/sass/main.sass')
-    .pipe(sass({ outputStyle: 'compressed' }))
-    .pipe(concat('style.min.css'))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 versions'],
-      grid: true
-    }))
-    .pipe(dest('app/css'))
-    .pipe(browserSync.stream())
+  return src("app/sass/main.sass")
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(concat("style.min.css"))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ["last 10 versions"],
+        grid: true,
+      })
+    )
+    .pipe(dest("app/css"))
+    .pipe(browserSync.stream());
 }
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
+    "node_modules/jquery/dist/jquery.js",
     // 'node_modules/jquery-match-height/dist/jquery.matchHeight.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'app/js/main.js'
+    "node_modules/slick-carousel/slick/slick.js",
+    "app/js/main.js",
   ])
-    .pipe(concat('main.min.js'))
+    .pipe(concat("main.min.js"))
     .pipe(uglify())
-    .pipe(dest('app/js'))
-    .pipe(browserSync.stream())
+    .pipe(dest("app/js"))
+    .pipe(browserSync.stream());
 }
 
 function images() {
-  return src('app/images/**/*.*')
-    .pipe(imagemin([
-      imagemin.gifsicle({ interlaced: true }),
-      imagemin.mozjpeg({ quality: 75, progressive: true }),
-      imagemin.optipng({ optimizationLevel: 5 }),
-      imagemin.svgo({
-        plugins: [
-          { removeViewBox: true },
-          { cleanupIDs: false }
-        ]
-      })
-    ]))
-    .pipe(dest('dist/images'))
+  return src("app/images/**/*.*")
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 75, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest("dist/images"));
 }
 
 function build() {
-  return src([
-    'app/**/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js'
-  ], { base: 'app' })
-    .pipe(dest('dist'))
+  return src(
+    [
+      "app/**/*.html",
+      "app/css/style.min.css",
+      "app/js/main.min.js",
+      "app/fonts/*.*",
+    ],
+    { base: "app" }
+  ).pipe(dest("dist"));
 }
 
 function cleanDist() {
-  return del('dist')
+  return del("dist");
 }
 
-
 function watching() {
-  watch(['app/sass/**/*.sass'], styles);
-  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
-  watch(['app/pug/**/*.pug'], pugGulp).on('change', browserSync.reload)
+  watch(["app/sass/**/*.sass"], styles);
+  watch(["app/js/**/*.js", "!app/js/main.min.js"], scripts);
+  watch(["app/pug/**/*.pug"], pugGulp).on("change", browserSync.reload);
 }
 
 exports.pugGulp = pugGulp;
@@ -100,5 +102,4 @@ exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
-exports.default = parallel(pugGulp, styles, scripts, browsersync, watching)
-
+exports.default = parallel(pugGulp, styles, scripts, browsersync, watching);
